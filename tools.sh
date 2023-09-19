1a1822754f301209551e1bc64cb0f805a720bc02
AbianSL <abisanle@gmail.com>
2023-09-19 03:27:56 +0100

    error fixed

--- tools.sh
+++ tools.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
 search_aur_manager() {
-    local aur_managers=("yay" "trizen" "paru")
-    
-    echo "Searching for a AUR manager..."
-    
-    sleep 1
+    local aur_managers=("yay" "trizen" "paru") 
 
     for manager in "${aur_managers[@]}"; do
         if command -v "$manager" &>/dev/null; then
-            echo "Found $manager as AUR manager."\n
+            echo "$manager"
             return
         fi
     done
     
-    echo "No AUR manager found."
+    echo "No AUR manager found." >&1
     
     sleep 1
 
-    echo "Do you want to install yay? (Type 'Yes' or 'Y' to confirm): " confirmation
+    read -p "Do you want to install yay? (Type 'Yes' or 'Y' to confirm): " confirmation
     confirmation="$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')"
     if [ "$confirmation" != "yes" ] && [ "$confirmation" != "y" ]; then
         echo "Installation aborted."
         exit 1
     fi
     
-    echo "Installing yay..."
     sudo pacman -S --needed yay
     
     sleep 1
-
-    echo "Installation of yay finished."
+    
+    echo "yay"
 }
 
 AUR_MANAGER="$(search_aur_manager)"
