d84b452d41583b9ea52d015d60ecea4cff9ace4c
AbianSL <abisanle@gmail.com>
2023-09-19 02:48:51 +0100

    added time to slow down

--- tools.sh
+++ tools.sh
@@ -5,6 +5,8 @@ search_aur_manager() {
     
     echo "Searching for a AUR manager..."
     
+    sleep 1
+
     for manager in "${aur_managers[@]}"; do
         if command -v "$manager" &>/dev/null; then
             echo "Found $manager as AUR manager."
@@ -14,6 +16,8 @@ search_aur_manager() {
     
     echo "No AUR manager found."
     
+    sleep 1
+
     echo "Do you want to install yay? (Type 'Yes' or 'Y' to confirm): " confirmation
     confirmation="$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')"
     if [ "$confirmation" != "yes" ] && [ "$confirmation" != "y" ]; then
@@ -24,6 +28,8 @@ search_aur_manager() {
     echo "Installing yay..."
     sudo pacman -S --needed yay
     
+    sleep 1
+
     echo "Installation of yay finished."
     return "yay"
 }
